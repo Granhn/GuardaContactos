f45bca777add381e974cb2ca56fb1d0e9c99db45
Granhn <fuenzaladeveloper@gmail.com>
2023-05-22 17:59:08 -0400

    Pinia instalado y probado tambien

--- FRONTEND/guarda-contactos-front/src/main.js
+++ FRONTEND/guarda-contactos-front/src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import router from './routes'
 import App from './App.vue'
 
+const pinia = createPinia();
 const app = createApp(App)
+app.use(pinia);
 app.use(router);
-app.mount('#app')
+app.mount('#app');
